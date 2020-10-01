ac5bfeebc4c5b84dbb632e0fe9c1bb259538f2ca
lovesa <samara.love@bhp.com>
2020-10-01 08:21:58 +0800

    Categories drop down working. Initial project,plege form

--- src/pages/CreatePledgePage.jsx
+++ src/pages/CreatePledgePage.jsx
@@ -1,12 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreatePledgePage() {
-  return <h1>This is the create pledge form.</h1>;
-  //   return (
-  //     <div id="about-page">
+  const [credentials, setCredentials] = useState({
+    amount: "",
+    comment: "",
+    anonymous: false,
+    // project_id: "",
+  });
 
-  //     </div>
-  //   );
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
+  };
+
+  const postData = async () => {
+    const response = await fetch(
+      //   `${process.env.REACT_APP_API_URL}api-token-auth/`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      }
+    );
+    return response.json();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (credentials.title && credentials.description) {
+      postData().then((response) => {
+        console.log(response);
+        // window.localStorage.setItem("token", response.token);
+      });
+    }
+  };
+  return (
+    //   <h1>This is the create pledge form.</h1>
+    <form>
+      <div>
+        <label htmlFor="amount">Amount:</label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Enter amount"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="comment">Comment:</label>
+        <input
+          type="text"
+          id="comment"
+          placeholder="Enter a comment"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="anonymous">Anonymous:</label>
+        <input
+          type="checkbox"
+          id="anonymous"
+          placeholder="anonymous"
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default CreatePledgePage;
--- src/pages/CreateProjectPage.jsx
+++ src/pages/CreateProjectPage.jsx
@@ -1,12 +1,146 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function CreateProjectPage() {
-  return <h1>This is the create project form.</h1>;
-  //   return (
-  //     <div id="about-page">
+  const [categoryData, setcategoryData] = useState([]);
+  //   const { id } = useParams();
 
-  //     </div>
-  //   );
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}categories/`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setcategoryData(data);
+        console.log(data);
+
+        // data.map((category) => console.log(category.category));
+      });
+  }, []);
+
+  const [credentials, setCredentials] = useState({
+    title: "",
+    description: "",
+    goal: 150,
+    image: "",
+    company: "",
+    deadline: "",
+    category: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setCredentials((prevCredentials) => ({
+      ...prevCredentials,
+      [id]: value,
+    }));
+  };
+
+  const postData = async () => {
+    const response = await fetch(
+      //   `${process.env.REACT_APP_API_URL}api-token-auth/`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      }
+    );
+    return response.json();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (credentials.title && credentials.description) {
+      postData().then((response) => {
+        console.log(response);
+        // window.localStorage.setItem("token", response.token);
+      });
+    }
+  };
+  //   console.log(data);
+  //   console.log(categoryData.category);
+  //   console.log(categoryData);
+  //   console.log(category);
+
+  return (
+    //   <h1>This is the create project form.</h1>
+    <form>
+      <div>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          placeholder="Enter title"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="description">Description:</label>
+        <input
+          type="text"
+          id="description"
+          placeholder="description"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="goal">Goal:</label>
+        <input
+          type="number"
+          id="goal"
+          placeholder="goal"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="image">Image:</label>
+        <input
+          type="url"
+          id="image"
+          placeholder="image"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="company">Company:</label>
+        <input
+          type="text"
+          id="company"
+          placeholder="company"
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="deadline">Deadline:</label>
+        <input
+          type="date"
+          id="deadline"
+          placeholder="deadline"
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="category">category:</label>
+        <select
+          type="select"
+          id="category"
+          placeholder="category"
+          onChange={handleChange}
+        >
+          {categoryData.map((cat) => (
+            <option key={cat.category} value={cat.category}>
+              {cat.category}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default CreateProjectPage;
